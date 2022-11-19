2a4ef0920cfcf205882dffdca4c91a6efa5dc498
StacyMch <116436690+StacyMch@users.noreply.github.com>
2022-11-19 08:41:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@ let templateCard = document.getElementById('tmpl-card').innerHTML;
         let info = {
 
         'name': 'Rick and Morty (TV series)',
-        'picture': '..//img/Rick_and_Morty_title_screen.webp',
+        'picture': 'img/Rick_and_Morty_title_screen.webp',
         'text': 'Rick and Morty is an American animated television series created by Dan Harmon and Justin Roiland that premiered on December 2, 2013 on Cartoon Network Adult Swim programming block. In Canada, it premiered on January 10, 2016 on the Canadian version of Cartoon Network Adult Swim programming block. The first season consists of 11 twenty-two minute episodes. After airing the first six episodes, [adult swim] renewed the show for a second season, consisting of 10 twenty-two minute episodes. The show was renewed for a third season, consisting of 10 twenty-two minute episodes that aired in the summer of 2017. As of Fall of 2019 Rick and Morty came back with a fourth season airing ten episodes with the first half of the season being air in 2019 and the second half after new years. The show was based on a series of crudely animated short films for Channel 101 based on a Back to the Future parody The Real Animated Adventures of Doc and Mharti by Justin Roiland.'
     
     }
